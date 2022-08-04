8b423f9bc06e8411899fcb014e6e9c0ec6218df6
AsiriSanda <sadakelum1234@gmail.com>
2022-08-04 20:02:38 +0530

    updated

---
+++ src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
@@ -0,0 +1,10 @@
+package lk.ijse.spring.advisor;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@CrossOrigin
+@RestControllerAdvice
+public class AppWideExceptionHandler {
+
+}
